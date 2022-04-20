39c796646a8e8f0e84500796979bb5e059d17ef5
Eleikel <victorkhalifa02@gmail.com>
2022-04-20 13:57:16 -0400

    see

--- Presentacion/Archivo/Entidades.cs
+++ Presentacion/Archivo/Entidades.cs
@@ -29,7 +29,7 @@ namespace Presentacion
 
         private void btnModificar_Click(object sender, EventArgs e)
         {
-            //Borrar este comentario
+            //Borrar este comentari
         }
 
         private void button1_Click(object sender, EventArgs e)
