dfe5948f380cf9657ebf6a5cb88ac08dffeb8713
Eleikel <victorkhalifa02@gmail.com>
2022-04-20 14:07:28 -0400

    prueba

---
+++ Presentacion/Archivo/Funcionalidades de Entidades/Borrar_este_form.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Presentacion.Archivo.Funcionalidades_de_Entidades
+{
+    public partial class Borrar_este_form : Form
+    {
+        public Borrar_este_form()
+        {
+            InitializeComponent();
+        }
+    }
+}
